9d31eb4b2158eda6766536075124ae438b5d8138
Lukas Metzmacher <lukas.metzmacher@bild.de>
2020-01-04 16:12:08 +0100

    created booking model and controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,30 @@
+class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(
+      :location, :start_date, :end_date
+      )
+  end
+end
