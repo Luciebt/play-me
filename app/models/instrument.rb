f68963894ea6a2c70f102fa63b393b45359cd407
Rokas Lukosius <tarakonas1@gmx.de>
2020-01-07 21:19:36 +0100

    minor changes

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,10 +1,12 @@
 class Instrument < ApplicationRecord
-  has_many :bookings, :reviews
+  has_many :bookings
+  # has_many :reviews
   belongs_to :user
 
-  # CATEGORIES = ['Strings', 'Keys', 'Woodwinds', 'Brass', 'Percusion'].freeze
+  CATEGORIES = ['Strings', 'Keys', 'Woodwinds', 'Brass', 'Percusion'].freeze
 
-  def self.categories
-    ['Strings', 'Keys', 'Woodwinds', 'Brass', 'Percusion']
-  end
+  # def self.categories
+  #   ['Strings', 'Keys', 'Woodwinds', 'Brass', 'Percusion']
+  # end
+  # enum category: [:strings, :keys, :woodwinds, :brass, :percussion]
 end
